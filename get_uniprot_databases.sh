be0c2dd5dc61a06351b4935b6f1a7388bdc4865e
Matin Gühmann <guehmann@t-online.de>
2021-06-19 18:15:02 +0100

    Makr scripts work when called from any location

--- get_uniprot_databases.sh
+++ get_uniprot_databases.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-./get_uniprot_sprot.sh &
-./get_uniprot_trembl.sh &
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+# Directory and the name of this script
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+"$DIR/get_uniprot_sprot.sh" &
+"$DIR/get_uniprot_trembl.sh" &
--- get_uniprot_sprot.sh
+++ get_uniprot_sprot.sh
@@ -1,5 +1,20 @@
 #/bin/bash
 
+# Get the directory where this script is
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+# Directory and the name of this script
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+mkdir -p "$DIR/uniprot_sprot"
+
+cd "$DIR/uniprot_sprot"
+
 wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 gunzip uniprot_sprot.fasta.gz
 makeblastdb -in uniprot_sprot.fasta -out uniprot_sprot -dbtype prot
